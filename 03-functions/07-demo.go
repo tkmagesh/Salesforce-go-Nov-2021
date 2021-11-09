1b6e453ac532cb83369fb244c18b60311b2d257d
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-09 21:30:13 +0530

    closures - assignment

---
+++ 03-functions/07-demo.go
@@ -0,0 +1,16 @@
+//closures
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(increment()) //=> 1
+	fmt.Println(increment()) //=> 2
+	fmt.Println(increment()) //=> 3
+	fmt.Println(increment()) //=> 4
+}
+
+func increment() int {
+
+}
