c14cb41b823b4879d59f7cb6dd9dd19764080edd
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-09 20:19:27 +0530

    functions intro

--- 02-programming-constructs/assignment.go
+++ 02-programming-constructs/assignment.go
@@ -1,38 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	for {
-		var choice int
+		var choice string
 		fmt.Println("Enter your choice:")
 		fmt.Println("1. Add")
 		fmt.Println("2. Subtract")
 		fmt.Println("3. Multiply")
 		fmt.Println("4. Divide")
 		fmt.Println("5. Exit")
-		fmt.Scanf("%d", &choice)
-		if choice == 5 {
+		fmt.Scanf("%s", &choice)
+		if choice == "5" {
 			break
 		}
-		if choice < 1 || choice > 5 {
+		if choice < "1" || choice > "5" {
 			fmt.Println("Invalid choice")
 			continue
 		}
-		var num1, num2 int
+		var num1, num2 float32
 		fmt.Println("Enter two numbers:")
-		fmt.Scanf("%d %d", &num1, &num2)
-		var result int
+		fmt.Scanf("%f %f", &num1, &num2)
+		var result float32
 		switch choice {
-		case 1:
+		case "1":
 			result = num1 + num2
-		case 2:
+		case "2":
 			result = num1 - num2
-		case 3:
+		case "3":
 			result = num1 * num2
-		case 4:
+		case "4":
 			result = num1 / num2
 		}
 		fmt.Println("Result: ", result)
 	}
+
 }
---
+++ 03-functions/01-demo.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(add(100, 200))
+	fmt.Println(divide(100, 7))
+}
+
+/*
+func add(x int, y int) int {
+	return x + y
+}
+*/
+
+/*
+func add(x, y int) int {
+	return x + y
+}
+*/
+
+func add(x, y int) (result int) {
+	result = x + y
+	return
+}
+
+/*
+func divide(x, y int) (int, int) {
+	quotient := x / y
+	remainder := x % y
+	return quotient, remainder
+}
+*/
+
+func divide(x, y int) (quotient, remainder int) {
+	quotient = x / y
+	remainder = x % y
+	return
+}
