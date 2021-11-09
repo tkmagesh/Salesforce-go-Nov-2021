32b1fd0d05ed1210a7951cb57066162795adc0f8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-09 19:31:22 +0530

    accepting user input & assignment

--- 01-intro/hello-world.go
+++ 01-intro/hello-world.go
@@ -14,6 +14,17 @@ func main() {
 	msg = "Welcome to Golang!!"
 	//fmt.Println("Hello World!")
 	fmt.Printf("Hello World! %s\n", msg)
+
+	//accepting input from user
+	var name string
+	fmt.Println("Enter the name :")
+	fmt.Scanf("%s\n", &name)
+	fmt.Printf("Hello %s\n", name)
+
+	var no int
+	fmt.Println("Enter the number :")
+	fmt.Scanf("%d\n", &no)
+	fmt.Printf("%d\n", no)
 }
 
 //other functions
