352c674cafa13ce7f218aa02d6aa0e8f0392c5b8
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-09 19:19:13 +0530

    using switch case & fallthrough

--- 02-programming-constructs/program.go
+++ 02-programming-constructs/program.go
@@ -42,4 +42,54 @@ func main() {
 		}
 	}
 	fmt.Println("x = ", x)
+
+	//switch case construct
+	/*
+		rank by score
+		score 0 - 3 => "Terrible"
+		score 4 - 7 => "Not Bad"
+		score 8 - 9 => "Good"
+		score 10 => "Excellent"
+		otherwise => "Unknown score"
+	*/
+
+	score := 6
+	switch score {
+	case 0, 1, 2, 3:
+		fmt.Println("Terrible")
+	case 4, 5, 6, 7:
+		fmt.Println("Not Bad")
+	case 8, 9:
+		fmt.Println("Good")
+	case 10:
+		fmt.Println("Excellent")
+	default:
+		fmt.Println("Unknown score")
+	}
+
+	//using fallthrough
+	n := 4
+	switch n {
+	case 0:
+		fmt.Println("is Zero")
+	case 1:
+		fmt.Println("n <= 1")
+		fallthrough
+	case 2:
+		fmt.Println("n <= 2")
+		fallthrough
+	case 3:
+		fmt.Println("n <= 3")
+		fallthrough
+	case 4:
+		fmt.Println("n <= 4")
+		fallthrough
+	case 5:
+		fmt.Println("n <= 5")
+		fallthrough
+	case 6:
+		fmt.Println("n <= 6")
+	case 7:
+		fmt.Println("n <= 7")
+	}
 }
