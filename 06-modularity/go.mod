06a18ad4b2b7b653658ce446f4cc780d1b5dea70
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-10 19:33:34 +0530

    modules demo

--- 06-modularity/go.mod
+++ 06-modularity/go.mod
@@ -1,3 +1,5 @@
 module modularity-demo
 
 go 1.16
+
+require github.com/fatih/color v1.13.0
--- 06-modularity/main.go
+++ 06-modularity/main.go
@@ -4,12 +4,15 @@ import (
 	"fmt"
 	"modularity-demo/calculator"
 	numUtils "modularity-demo/calculator/utils"
+
+	"github.com/fatih/color"
 )
 
 func main() {
 	fmt.Println(calculator.Add(10, 20))
 	fmt.Println(calculator.Subtract(10, 20))
 	/* fmt.Println(calculator.multiply(10, 20)) */
-	fmt.Println("Op count =", calculator.GetOpCount())
+	color.Yellow("Op count = %d\n", calculator.GetOpCount())
+	//fmt.Printf("Op count = %d\n", calculator.GetOpCount())
 	fmt.Println(numUtils.IsEven(201))
 }
