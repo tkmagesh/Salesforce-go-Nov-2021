72c1c62d92f83c0fb06ce5ca859251f5d12dfa4a
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-10 16:09:57 +0530

    slices

--- 04-collections/program.go
+++ 04-collections/program.go
@@ -28,4 +28,57 @@ func main() {
 	for _, value := range nos {
 		fmt.Println(value)
 	}
+
+	fmt.Println("Copying an array")
+	var newNos = nos
+	newNos[0] = 100
+	fmt.Printf("nos = %v, newNos = %v\n", nos, newNos)
+
+	//Slice
+	fmt.Println("Slice")
+	var products []string
+	//var products []string = []string{"Laptop", "Mobile", "TV", "Camera"}
+	fmt.Println(products)
+
+	/*
+		products = append(products, "Laptop")
+		products = append(products, "Mobile")
+	*/
+
+	products = append(products, "Laptop", "Mobile")
+	fmt.Println(products)
+
+	stationaryProducts := make([]string, 2)
+	stationaryProducts[0] = "Pen"
+	stationaryProducts[1] = "Pencil"
+	//stationaryProducts := []string{"Pen", "Pencil"}
+	products = append(products, stationaryProducts...)
+	fmt.Println(products)
+
+	fmt.Println("Len & capacity")
+	fmt.Printf("len(stationaryProducts) = %d, cap(stationaryProducts) = %d\n", len(stationaryProducts), cap(stationaryProducts))
+	fmt.Println("After appending 'Marker' to stationary products")
+	stationaryProducts = append(stationaryProducts, "Marker")
+	fmt.Printf("len(stationaryProducts) = %d, cap(stationaryProducts) = %d\n", len(stationaryProducts), cap(stationaryProducts))
+	stationaryProducts = append(stationaryProducts, "Scribble-Pad")
+	fmt.Printf("len(stationaryProducts) = %d, cap(stationaryProducts) = %d\n", len(stationaryProducts), cap(stationaryProducts))
+	stationaryProducts = append(stationaryProducts, "Ruler")
+	fmt.Printf("len(stationaryProducts) = %d, cap(stationaryProducts) = %d\n", len(stationaryProducts), cap(stationaryProducts))
+
+	/* slicing */
+	/*
+		[lo : hi] => from lo to hi-1
+		[lo : ] => from lo to end
+		[ : hi] => from 0 to hi-1
+		[lo : lo] => empty slice
+		[lo : lo + 1] === [lo] => data at lo
+		[:] => copy of the slice
+		[lo : hi step step] => from lo to hi-1 with step
+	*/
+	fmt.Println("Slicing")
+	fmt.Println("Stationary Products => ", stationaryProducts)
+	fmt.Println("Stationary Products[1:3] => ", stationaryProducts[1:3])
+	fmt.Println("Stationary Products[2 :] => ", stationaryProducts[2:])
+	fmt.Println("Stationary Products[:2] => ", stationaryProducts[:2])
+
 }
