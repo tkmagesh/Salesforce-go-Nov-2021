1760547d3e118d6abd533212629fb337beb7ba64
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-10 20:07:57 +0530

    struct composition

---
+++ 07-structs/composition.go
@@ -0,0 +1,31 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float64
+	Units    int
+	Category string
+}
+
+type PerishableProduct struct {
+	Product
+	Id     int
+	Expiry string
+}
+
+func main() {
+	//creating an instance
+	grapes := PerishableProduct{Product: Product{201, "Grapes", 5.0, 20, "Fruit"}, Expiry: "2 Days"}
+	fmt.Printf("%#v\n", grapes)
+
+	fmt.Println(grapes.Product.Id, grapes.Name, grapes.Cost)
+
+	//use the applyDiscount function to apply 20% discount the 'grapes'
+}
+
+func applyDiscount(product *Product, discount float64) {
+	product.Cost *= (1 - discount)
+}
