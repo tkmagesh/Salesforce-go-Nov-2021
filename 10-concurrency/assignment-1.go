2bcc016125cdd3c7085c3bf36194676c42ca9bd4
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-11 20:01:15 +0530

    assignment-1 solution

--- 10-concurrency/assignment-1.go
+++ 10-concurrency/assignment-1.go
@@ -1,19 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+	"time"
+)
 
 func main() {
-	printString("Hello")
-	printString("World")
+	wg := &sync.WaitGroup{}
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+	wg.Add(2)
+	go printString("Hello", ch1, ch2, wg)
+	go printString("World", ch2, ch1, wg)
+	ch1 <- "start"
+	wg.Wait()
 }
 
-func printString(s string) {
+func printString(s string, in chan string, out chan string, wg *sync.WaitGroup) {
+	defer wg.Done()
 	for i := 0; i < 5; i++ {
+		time.Sleep(1 * time.Second)
+		<-in
 		fmt.Println(s)
+		out <- "done"
 	}
 }
 
-/* 
+/*
 Expected Output:
 Hello
 World
@@ -27,5 +41,4 @@ Hello
 World
 
 Note : DONOT move the for loop outside the printString function.
-*/
 */
