4643272aa555a7bf913a3d3ed8337a22e1e52f33
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-11 20:04:08 +0530

    Create assignment-2.go

---
+++ 10-concurrency/assignment-2.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+	data := []int{4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55, 4, 1, 7, 3, 8, 5, 9, 3, 11, 15, 63, 87, 29, 40, 50, 55}
+
+	fmt.Println("Sum = ", sum(data...))
+}
+
+func sum(nos ...int) int {
+	var result int
+	for _, v := range nos {
+		result += v
+	}
+	return result
+}
+
+/*
+	Divide & Conquer
+	Break the dataset into 2 sets, add them using 2 goroutines and print the final result
+*/
