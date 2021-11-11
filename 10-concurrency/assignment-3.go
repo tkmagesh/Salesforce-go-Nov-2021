80be661760bf19d174863f50fe5976db72e79249
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-11 21:33:15 +0530

    Create assignment-3.go

---
+++ 10-concurrency/assignment-3.go
@@ -0,0 +1,45 @@
+/* Modify this program in such a way that it keeps generting the fibonacci numbers until the users hits enter key */
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	go fibonacci(ch)
+	for no := range ch {
+		fmt.Println(no)
+	}
+}
+
+func fibonacci(ch chan int) {
+	done := time.After(10 * time.Second)
+
+	/*
+		done := func() chan string {
+			ch := make(chan string)
+			go func() {
+				time.Sleep(10 * time.Second)
+				ch <- "done"
+			}()
+			return ch
+		}()
+	*/
+
+	x, y := 0, 1
+
+	for {
+		select {
+		case ch <- x:
+			x, y = y, x+y
+			time.Sleep(time.Millisecond * 500)
+		case <-done:
+			fmt.Println("Exiting from fibonacci")
+			close(ch)
+			return
+		}
+	}
+}
