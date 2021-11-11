0ce99b338e92cf6e38dc812f10a0caf07c88d426
Magesh Kuppan <tkmagesh77@gmail.com>
2021-11-11 15:35:54 +0530

    using Stringer interface

---
+++ 09-interfaces/02-demo.go
@@ -0,0 +1,45 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	Id       int
+	Name     string
+	Cost     float64
+	Units    int
+	Category string
+}
+
+type PerishableProduct struct {
+	Product
+	Expiry string
+}
+
+func main() {
+
+	product := Product{Id: 100, Name: "Pen", Cost: 5.0, Units: 10, Category: "Stationery"}
+	//print(product)
+	fmt.Println(product)
+
+	fmt.Printf("Before applying discount : \n%#v\n", product)
+	product.applyDiscount(0.50)
+	fmt.Printf("After applying discount : \n%#v\n", product)
+
+	grapes := PerishableProduct{Product: Product{201, "Grapes", 5.0, 20, "Fruit"}, Expiry: "2 Days"}
+	grapes.applyDiscount(0.50)
+	//fmt.Printf("%#v\n", grapes)
+	fmt.Println(grapes)
+}
+
+//implementation of the "Stringer" interface
+func (product Product) String() string {
+	return fmt.Sprintf("Id = %d, Name = %s, Cost = %f, Units = %d, Category = %s\n", product.Id, product.Name, product.Cost, product.Units, product.Category)
+}
+
+func (product *Product) applyDiscount(discount float64) {
+	product.Cost *= (1 - discount)
+}
+
+func (pp *PerishableProduct) String() string {
+	return fmt.Sprintf("%s, Expiry = %s\n", pp.Product, pp.Expiry)
+}
